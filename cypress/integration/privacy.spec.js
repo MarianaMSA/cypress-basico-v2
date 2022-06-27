ad46840164249c51f769de8f99036f398212e854
MarianaMSA <maiasalvador@gmail.com>
2022-06-26 21:24:17 -0300

    scripts e headless

--- cypress/integration/privacy.spec.js
+++ cypress/integration/privacy.spec.js
@@ -2,7 +2,7 @@ it.only("testa a página da política de privavidade de forma independente - W",
   cy.visit("./src/privacy.html");
   cy.title().should(
     "be.equal",
-    "Central de Atendimento ao Cliente TAT - Política de privacidade"
+    "Central de Atendimento a Cliente TAT - Política de privacidade"
   );
   cy.contains("HTML, CSS e JavaScript").should("be.visible");
 });
